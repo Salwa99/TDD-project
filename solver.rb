f14ca46953cb8936c9d0620121be40ad04a111ed
Salwa99 <s.ritage99@gmail.com>
2023-03-22 20:31:14 +0000

    Create test file for class Solver

--- solver.rb
+++ solver.rb
@@ -1,14 +1,3 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, "Input must be a non-negative number!" unless n.is_a?(Integer) && n >= 0
     
-        return 1 if n == 0
-    
-        result = 1
-        (1..n).each do |i|
-          result *= i
-        end
-    
-        return result
-      end
     end
